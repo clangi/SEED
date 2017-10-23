188d13f76954cc0e8d514be7ee1d62fbd2d479e5
clangini <cassiano.langini@gmail.com>
2017-10-23 20:48:34 +0200

    Added translation step

--- src/montecarlo.cpp
+++ src/montecarlo.cpp
@@ -14,3 +14,14 @@ void rot_move(double** RoSFCo, int FrAtNu, Parameter & seed_par) {
     q.quatConjugateVecRef(RoSFCo[i], ct);
   }
 }
+
+void trans_move(double **coord, int num_at, Parameter& par) {
+  double t[4];
+  for (int i = 1; i <= 3; i++)
+    t[i] = rnd_gen::get_uniform(0, par.mc_max_tran_step);
+  for (int i = 1; i <= num_at; i++){
+    coord[i][1] = coord[i][1] + t[1];
+    coord[i][2] = coord[i][2] + t[2];
+    coord[i][3] = coord[i][3] + t[3];
+  }
+}
--- src/montecarlo.h
+++ src/montecarlo.h
@@ -4,6 +4,7 @@
 #include "quaternion.h"
 #include "Parameter.h"
 
-void rot_move(double** RoSFCo, int FrAtNu, Parameter & seed_par);
+void rot_move(double **RoSFCo, int FrAtNu, Parameter& seed_par);
+void trans_move(double **coord, int num_at, Parameter& par);
 
 #endif
